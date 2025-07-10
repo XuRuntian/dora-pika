b41e202fa7329a377902cce627d36180ead92464
XuRuntian <xuruntian03@163.com>
2025-07-10 16:48:32 +0800

    添加dora-glsight的sdk文件

---
+++ dora-dm-tac/src/DM-Tac WM/README.md
@@ -0,0 +1,13 @@
+# Interface for Daimon tactile sensor
+
+# How to use
+
+## Work with Python 3.8/3.9/3.10. Make sure you have cuda toolkit 12.x installed, otherwise you might need to modify setup.py
+
+## Install the package
+    pip install .
+
+## Plug in the sensor
+
+## Run
+    python main.py
---
+++ dora-dm-tac/src/DM-Tac WM/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dmrobotics",
+    version="0.1.3",
+    python_requires=">=3.8, <3.11",
+    packages=find_packages(),
+    license="",
+    author="Yipai Du",
+    author_email="yipai.du@outlook.com",
+    install_requires=[
+        "numpy==1.24.4",
+        "opencv-python==4.10.0.84",
+        "opencv-contrib-python==4.10.0.84",
+        "scipy==1.10.1",
+        "setuptools==45.2.0",
+        "cupy-cuda12x==12.3.0",
+        'pyudev; platform_system=="Linux"',  # For Linux platform
+    ],
+    package_data={"": ["*.so"]},
+    include_package_data=True,
+    description="Tactile sensor interface for Daimon Robotics.",
+)
